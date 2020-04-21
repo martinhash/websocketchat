180d9eebced0fb556fe08223f38a3c64887e470b
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-21 14:15:51 -0600

    feat socket.io

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio.listen(server);
 
+io.on('connection', socket=>{
+    console.log("New user connected");
+})
+
+
+//STATIC FILES
 app.use(express.static('public'));
 
-
-app.listen(3000, ()=>{
+//START SERVER
+server.listen(3000, ()=>{
     console.log("Server on port: 3000");
 })
 
