18ef23cff6d4d236066b5fd7fa18d3b377a97982
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-22 19:05:52 -0600

    array users

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -8,6 +8,30 @@ $(function (){
     const $messageBox = $('#message');
     const $chat  = $('#chat');
 
+    const $nickForm = $('#nickForm');
+    const $nickError = $('#nickError');
+    const $nickName = $('#nickName');
+
+    const $users = $('#userNames');
+    
+
+    $nickForm.submit(e=>{
+        e.preventDefault();
+        socket.emit('new user', $nickName.val(), data =>{
+            if(data){
+                $('#nickWrap').hide();
+                $('#contentWrap').show();
+            }else{
+                $nickError.html(`
+                    <div class="alert alert-danger">
+                        That user already exists.
+                    </div>
+                `);
+                $nickName.val('');
+            }
+        });
+    })
+    
     //EVENTS
     $messageForm.submit( (e)=>{
         e.preventDefault();
@@ -19,5 +43,17 @@ $(function (){
         $chat.append(data + '<br/>')
     })
 
+    socket.on('usernames', data =>{
+        let html = '';
+        for(let i = 0; i < data.length; i++){
+            html += `
+                <p>
+                <i class="fas fa-user"></i>
+                ${data[i]}
+                </p>
+            `
+        }
+        $users.html(html);
+    })
 
 })
--- src/sockets.js
+++ src/sockets.js
@@ -1,9 +1,29 @@
 module.exports = function(io){
+
+
+    let nickNames = [];
+
     io.on('connection', socket=>{
+
+
+        socket.on('new user', function(data, cb){
+            console.log(data);
+            
+            if(nickNames.indexOf(data) != -1){
+                cb(false);
+            }else{
+                cb(true);
+                socket.nickName = data;
+                nickNames.push(socket.nickName);
+                io.sockets.emit('usernames', nickNames);
+            }
+        });
 
         socket.on('send message', function(data){
             io.sockets.emit('new message', data)
-        })
+        });
+
+
 
     })
 }
